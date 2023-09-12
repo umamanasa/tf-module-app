9c38cda92b851b14f960abc31347091343284595
umamanasa <umamanasa49@gmail.com>
2023-09-12 17:19:58 +0530

    adding logstash,filebeat conf setup files in roboshop

--- main.tf
+++ main.tf
@@ -166,6 +166,16 @@ resource "aws_lb_target_group" "main" {
   port     = var.port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/health"
+    port                = var.port
+    timeout             = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_listener_rule" "main" {
@@ -185,15 +195,27 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-  count    = var.component == "frontend" ? 1 : 0
-  name     = "${local.name_prefix}-public"
-  port     = var.port
-  target_type = "ip"
-  protocol = "HTTP"
-  vpc_id   = var.default_vpc_id
+  count         = var.component == "frontend" ? 1 : 0
+  name          = "${local.name_prefix}-public"
+  port          = var.port
+  target_type   = "ip"
+  protocol      = "HTTP"
+  vpc_id        = var.default_vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 5
+    path                = "/"
+    port                = var.port
+    timeout             = 2
+    unhealthy_threshold = 2
+    matcher             = "404"
+  }
 }
 resource "aws_lb_target_group_attachment" "public" {
-  count             = var.component == "frontend" ? length(var.subnet_ids) : 0
+  count             = var.component == "frontend" ? length(tolist(data.dns_a_record_set.private_alb.addrs)) : 0
+#  count             = var.component == "frontend" ? length(var.subnet_ids) : 0
   target_group_arn  = aws_lb_target_group.public[0].arn
   target_id         = element(tolist(data.dns_a_record_set.private_alb.addrs), count.index )
   port              = 80
