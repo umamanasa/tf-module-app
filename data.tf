da74a36ec673c3fbf7d105b132d663282a3c10bb
umamanasa <umamanasa49@gmail.com>
2023-09-29 15:50:13 +0530

    creating own roboshop-ami

--- data.tf
+++ data.tf
@@ -4,6 +4,13 @@ data "aws_ami" "ami" {
   owners      = ["973714476881"]
 }
 
+## Creating Own Ami
+#data "aws_ami" "ami" {
+#  most_recent = true
+#  name_regex  = "roboshop-ami-v1"
+#  owners      = [""]
+#}
+
 data "dns_a_record_set" "private_alb" {
   host = var.private_alb_name
 }
